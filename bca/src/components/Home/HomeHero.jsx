cb98fe8045501bf22626ab94273a9bac3156bf27
Ganta Kaushik <89829656+Gantakaushik@users.noreply.github.com>
2024-05-06 18:16:30 +0530

    Add existing project files to Git

---
+++ bca/src/components/Home/HomeHero.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import "../../styles/Home/homehero.css";
+import Typewriter from 'typewriter-effect';
+// import image from '../assets/images/Onlin'
+const HomeHero = () => {
+  const images = [
+    {
+      title: "learn",
+      image:
+        "https://cdn.unstop.com/uploads/images/home/home-hero-learn.png?d=324x406",
+      link: "https://unstop.com/courses",
+    },
+    {
+      title: "practice",
+      image:
+        "https://cdn.unstop.com/uploads/images/home/home-hero-practice.png?d=324x406",
+      link: "https://unstop.com/practice",
+    },
+    {
+      title: "mentorships",
+      image:
+        "https://cdn.unstop.com/uploads/images/home/home-hero-mentorships.png?d=324x406",
+      link: "https://unstop.com/mentor",
+    },
+    {
+      title: "compete",
+      image:
+        "https://cdn.unstop.com/uploads/images/home/home-hero-compete.png?d=324x406",
+      link: "https://unstop.com/compete",
+    },
+    {
+      title: "jobs",
+      image:
+        "https://cdn.unstop.com/uploads/images/home/home-hero-jobs.png?d=324x406",
+      link: "https://unstop.com/job-portal",
+    },
+    {
+      title: "blogs",
+      image:
+        "https://cdn.unstop.com/uploads/images/home/home-hero-blogs.png?d=324x406",
+      link: "https://unstop.com/blog",
+    },
+  ];
+  
+  return (
+    <section className="home-hero">
+      <div className="home-hero-container container">
+        <div className="home-hero-left">
+          <h1>
+            <strong>
+            Empowering Futures<br/>
+            by Connecting
+            </strong>
+
+          </h1>
+          <p>
+          <span className="type-write-span">
+            <strong>
+            <Typewriter
+              options={{
+                strings: ['Students, Colleges, Recruiters!'],
+                autoStart: true,
+                loop: true,
+                
+              }}
+              />
+              </strong>
+              </span>
+          </p>
+        </div>
+        <div className="home-hero-right-image">
+          <div className="home-hero-right-images">
+            <img className="home-hero-image" src=''></img>
+          </div> 
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeHero;
